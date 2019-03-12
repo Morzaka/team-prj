360f9940ca1afcf47d95ec724a3849641940f508
dmytro <dovgal.dmytr@gmail.com>
2019-03-12 18:35:55 +0200

    Added config and log files

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 	This code just for testing and setting up Docker
 */
@@ -66,3 +67,53 @@ func waitForShutdown(srv *http.Server) {
 	log.Println("Shutting down")
 	os.Exit(0)
 }
+=======
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"team-project/config"
+)
+
+func main(){
+
+	configFile := flag.String("config", "./package_config.json", "Configuration file in JSON-format")
+	flag.Parse()
+
+	if len(*configFile) > 0 {
+		config.FilePath = *configFile
+	}
+
+	err := config.Load()
+	if err != nil {
+		log.Fatalf("error while reading config: %s", err)
+	}
+
+	f, err := os.OpenFile(config.Config.LogFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			log.Fatalf("error while close log file: %s", err)
+		}
+	}()
+
+	log.SetOutput(f)
+
+
+
+	log.Println("Starting HTTP listener...")
+	err = http.ListenAndServe(config.Config.ListenURL, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("Stop running application: %s", err)
+}
+
+>>>>>>> config
