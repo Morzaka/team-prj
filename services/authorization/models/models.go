32a109cf2fbdc77bc15f50d5d8b080a38e69ae0e
littleskew <littleskew@gmail.com>
2019-03-14 17:57:07 +0200

    added authorization

---
+++ services/authorization/models/models.go
@@ -0,0 +1,17 @@
+package models
+
+import "github.com/satori/go.uuid"
+type User struct {
+	Id       uuid.UUID
+	Password string
+	Name     string
+	Surname  string
+	Login    string
+	Role     string
+}
+
+
+func NewUser(id uuid.UUID,password, name,surname,login,role string) *User {
+	return &User{id, password, name,surname,login,role}
+}
+
