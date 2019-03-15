8865dce8c8d69b4e6b30c6d4ef6d86f1274008f5
littleskew <littleskew@gmail.com>
2019-03-15 23:56:06 +0200

    added middleware to new config file

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"flag"
 	"fmt"
 	"github.com/urfave/negroni"
-        "gitlab.com/golang-lv-388/team-project/services"
+        "team-project/services"
 	"net/http"
 	"team-project/configurations"
 	"team-project/logger"
@@ -20,43 +19,15 @@ func main(){
 		fmt.Println("Config file was read")
 	}
 	err := configurations.LoadConfig(*configFile)
-=======
-	"github.com/sirupsen/logrus"
-	"github.com/urfave/negroni"
-	"net/http"
-	"os"
-	"team-project/config"
-	"team-project/services"
 )
 
-func main() {
-	err := config.ReadAndLoad("project_config.json")
-	f, err := os.OpenFile(config.Config.LogFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
->>>>>>> routing
 	if err != nil {
 		logger.LogFatal("Fatal error while reading config, %s", err)
 	}
-<<<<<<< HEAD
 	middlewareManager.Use(negroni.NewRecovery())
         middlewareManager.UseHandler(services.NewRouter())
 	logger.LogInfo("Starting HTTP listener...")
 	err = http.ListenAndServe(configurations.Config.ListenURL, middlewareManager)
-=======
-	defer func() {
-		err = f.Close()
-		if err != nil {
-			logrus.Fatal("Error while closing log file", err)
-		}
-	}()
-
-	logrus.SetOutput(f)
-	// setting up web server middlewares
-	middlewareManager := negroni.New()
-	middlewareManager.Use(negroni.NewRecovery())
-	middlewareManager.UseHandler(services.NewRouter())
-	logrus.Info("Starting HTTP listening...")
-	err = http.ListenAndServe(config.Config.ListenURL, middlewareManager)
->>>>>>> routing
 	if err != nil {
 		logger.LogError("Error, %s",err)
 	}
