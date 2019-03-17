c8dd6941fab511a92272de6364bd0bb1e98dfa6f
littleskew <littleskew@gmail.com>
2019-03-17 09:16:49 +0000

    modified authorization

--- services/authorization/session/sessio.go
+++ services/authorization/session/sessio.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+//GenerateId generates id for the session
 func GenerateId() string {
 	b := make([]byte, 16)
 	rand.Read(b)
@@ -18,19 +19,20 @@ type sessionData struct {
 type Session struct {
 	Data map[string]*sessionData
 }
-
+//NewSession creates new session
 func NewSession() *Session {
 	obj := new(Session)
 	obj.Data = make(map[string]*sessionData)
 	return obj
 }
-
+//Method Init initialize the session
 func (obj *Session) Init(username string) string {
 	sessionId := GenerateId()
 	data := &sessionData{username}
 	obj.Data[sessionId] = data
 	return sessionId
 }
+//GetUser method returns authorized username
 func (obj *Session) GetUser(sessionId string) string {
 	data := obj.Data[sessionId]
 	if data == nil {
