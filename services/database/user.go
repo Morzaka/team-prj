ed667e7cea1712b03fd6950c47f71d79cb3f5d6e
littleskew <littleskew@gmail.com>
2019-03-21 00:33:20 +0200

    separate bd connection

--- services/database/user.go
+++ services/database/user.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	 "team-project/services/authorization/models"
-	 "team-project/services/database"
 )
 
 //AddUser adds info about new user to the database
@@ -15,7 +14,7 @@ func AddUser(user models.User) int {
         VALUES ($1, $2, $3, $4, $5)
         RETURNING id`
         id := 0
-        err = db.QueryRow(sqlStatement, user.Name, user.Surname, user.Login, user.Password, user.Role).Scan(&id)
+        err := db.QueryRow(sqlStatement, user.Name, user.Surname, user.Login, user.Password, user.Role).Scan(&id)
         if err != nil {
                 panic(err)
         }
@@ -29,7 +28,7 @@ func GetUserPassword(login string) string {
 	defer db.Close()
         //get user's password for given login
         sqlStatement := `SELECT password FROM users WHERE login=$1;`
-        err = db.QueryRow(sqlStatement, login).Scan(&password)
+        err := db.QueryRow(sqlStatement, login).Scan(&password)
         //if there's no matches for login return empty value
         if err != nil {
                  return ""
