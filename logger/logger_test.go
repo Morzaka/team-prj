8e5754ca020dd115f8729b8a9440aa30efbacf89
dmytro <dovgal.dmytr@gmail.com>
2019-03-26 17:16:05 +0200

    fixed logg test

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -10,19 +10,19 @@ func TestLoadLogWithWrongParseLevel(t *testing.T) {
 	LogFileName := "project_log_file.log"
 	err := LoadLog(LogFileName)
 	if err == nil {
-		t.Errorf("Testing logging file was failed, %s", err)
+		t.Errorf("Expected error, got %s", err)
 	}
 }
 
 //TestLoadLog will test our LoadLog function
 func TestLoadLog(t *testing.T) {
-	err := configurations.LoadConfig("../project_config.json")
-	if err != nil {
-		t.Errorf("Testing logging file was failed, LogLevel didn't read %s", err)
+	configurations.Config = configurations.Configuration{
+		LogLevel: "error",
 	}
+
 	LogFileName := "project_log_file.log"
-	err = LoadLog(LogFileName)
+	err := LoadLog(LogFileName)
 	if err != nil {
-		t.Errorf("Testing logging file was failed, %s", err)
+		t.Errorf("Expected empty error, got  %s", err)
 	}
 }
