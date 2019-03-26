205f278678dbfffcd09af6842b386f689acdb382
dmytro <dovgal.dmytr@gmail.com>
2019-03-26 14:57:06 +0200

    added test

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -5,6 +5,15 @@ import (
 	"testing"
 )
 
+//TestLoadLogWithWrongParseLevel will test our LoadLog function with wrong ParseLevel
+func TestLoadLogWithWrongParseLevel(t *testing.T) {
+	LogFileName := "project_log_file.log"
+	err := LoadLog(LogFileName)
+	if err == nil {
+		t.Errorf("Testing logging file was failed, %s", err)
+	}
+}
+
 //TestLoadLog will test our LoadLog function
 func TestLoadLog(t *testing.T) {
 	err := configurations.LoadConfig("../project_config.json")
