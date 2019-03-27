16f522c37a604402d9c38e57a28c23e65f0b1278
Morzaka <vujko007@gmail.com>
2019-03-27 17:33:23 +0200

    add booking

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module team-project
 
+go 1.12
+
 require (
 	github.com/go-redis/redis v6.15.2+incompatible
 	github.com/go-zoo/bone v0.0.0-20190117145001-d7ce1372afa7
@@ -7,7 +9,7 @@ require (
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/sirupsen/logrus v1.4.0
-	github.com/urfave/negroni v1.0.0
+	github.com/urfave/negroni v1.0.0 // indirect
 	github.com/x-cray/logrus-prefixed-formatter v0.5.2
-	golang.org/x/crypto v0.0.0-20190320223903-b7391e95e576
+	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c
 )
---
+++ services/booking/ticket.go
@@ -0,0 +1,26 @@
+package booking
+
+//Get place in train car
+
+//Train ID, Train routing, Date(Dep/Ariv), Time(Dep/Ariv), Duration(Time), Free Places(quantitu, num)
+// select place num, quantity of selecting place
+
+//Rout
+// api/v1/select_place/ GET  {return quantity & number of palaces}
+// api/v1/ticket_order/ POST {
+//                send: book place in a train}
+//             receive: pas1{UserName, TrainNum}
+//                      pas2{UserName, TrainNum}
+//                      pas3{UserName, TrainNum}
+//                      pas4{UserName, TrainNum}
+// api/v1/bucket/tickets/ GET
+//                           pas1{UserName, TrainNum, Price}
+//...                        pas2{UserName, TrainNum, Price}
+//...........................SUM price
+//                           set in Redis Timer 15min
+// api/v1/bucket/tickets/id DELETE return
+//...                        pas2{UserName, TrainNum, Price}
+//...........................delete in redis
+//                           Redis Timer continue
+//.api/v1/payment/tickets/id
+
