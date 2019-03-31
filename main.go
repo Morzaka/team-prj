a7e387a959df4329134374b6f60518af2956b18a
littleskew <littleskew@gmail.com>
2019-03-31 12:16:42 +0000

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"team-project/configurations"
 	"team-project/logger"
@@ -16,7 +17,7 @@ import (
 
 func main() {
 	//Flags for LoadLog and LoadConfig functions
-//	port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	configFile := flag.String("config", "./project_config.json", "Configuration file in JSON-format")
 	logFile := flag.String("logFile", "project_log_file.log", "Logging out file .log")
 	flag.Parse()
@@ -39,7 +40,7 @@ func main() {
 	middlewareManager.UseHandler(services.NewRouter())
 	fmt.Println("Starting HTTP listener...")
 	//Starting server
-	err = http.ListenAndServe(configurations.Config.ListenURL, middlewareManager)
+	err = http.ListenAndServe(":"+ port, middlewareManager)
 	if err != nil {
 		logger.Logger.Errorf("Error, %s", err)
 	}
