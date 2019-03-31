9af0538d9954cbd1df61d4f2d9f20aed2fa6d586
littleskew <littleskew@gmail.com>
2019-03-31 07:08:59 +0300

    del port in main

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"os"
 	"team-project/configurations"
 	"team-project/logger"
 	"team-project/services"
@@ -14,7 +13,6 @@ import (
 
 func main() {
 	//Flags for LoadLog and LoadConfig functions
-	port := os.Getenv("PORT")
 	configFile := flag.String("config", "./project_config.json", "Configuration file in JSON-format")
 	logFile := flag.String("logFile", "project_log_file.log", "Logging out file .log")
 	flag.Parse()
@@ -37,7 +35,7 @@ func main() {
 	middlewareManager.UseHandler(services.NewRouter())
 	fmt.Println("Starting HTTP listener...")
 	//Starting server
-	err = http.ListenAndServe(":"+port, middlewareManager)
+	err = http.ListenAndServe(configurations.Config.ListenURL, middlewareManager)
 	if err != nil {
 		logger.Logger.Errorf("Error, %s", err)
 	}
