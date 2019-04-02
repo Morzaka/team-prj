771368107e87d5d81be3c10c5c020c8bf0fd2620
littleskew <littleskew@gmail.com>
2019-04-02 10:54:55 +0000

    Merge branch 'master' of https://gitlab.com/golang-lv-388/team-project "pulling last version of master"

---
+++ database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	//pq lib for using postgres
+	_ "github.com/lib/pq"
+
+	"team-project/configurations"
+)
+
+//Db is a pointer to opened database
+var Db *sql.DB
+
+//PostgresInit connects to postgres database
+func PostgresInit() error {
+	//database connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
+		configurations.Config.PgHost, configurations.Config.PgPort, configurations.Config.PgUser, configurations.Config.PgPassword, configurations.Config.PgName)
+	//connect to database
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return err
+	}
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	Db = db
+	return nil
+}
--- services/router.go
+++ services/router.go
@@ -3,7 +3,7 @@ package services
 import (
 	"github.com/go-zoo/bone"
 	"team-project/services/authorization"
-	"team-project/services/handlers"
+	"team-project/services/model"
 )
 
 //NewRouter creates a router for URL
@@ -12,10 +12,11 @@ func NewRouter() *bone.Mux {
 	subV1 := bone.New()
 	router.SubRoute("/v1", subV1)
 	// GetFunc, PostFunc etc ... takes http.HandlerFunc
-	subV1.GetFunc("/startpage", handlers.GetStartFunc)
-	subV1.PostFunc("/signup", authorization.SignupFunc)
-	subV1.PostFunc("/signin", authorization.SigninFunc)
-	subV1.GetFunc("/login", authorization.LoginPage)
-	subV1.GetFunc("/register", authorization.RegisterPage)
+	subV1.GetFunc("/startpage", model.GetStartFunc)
+	subV1.PostFunc("/register", authorization.SignupFunc)
+	subV1.PostFunc("/login", authorization.SigninFunc)
+	subV1.PostFunc("/logout", authorization.LogoutFunc)
+	subV1.DeleteFunc("/user/:id", authorization.DeletePageFunc)
+	subV1.PatchFunc("/user/:id", authorization.UpdatePageFunc)
 	return router
 }
