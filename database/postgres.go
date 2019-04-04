db95a41cdbc9b826d546e4905afdb02e1877516f
supermaaan99 <candyluckone@yahoo.com.ua>
2019-04-04 11:08:39 +0300

    Modified database/postgress for deploy purposes

--- database/postgres.go
+++ database/postgres.go
@@ -2,12 +2,10 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
+	"os"
 
 	//pq lib for using postgres
 	_ "github.com/lib/pq"
-
-	"team-project/configurations"
 )
 
 //Db is a pointer to opened database
@@ -16,10 +14,10 @@ var Db *sql.DB
 //PostgresInit connects to postgres database
 func PostgresInit() error {
 	//database connection string
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
-		configurations.Config.PgHost, configurations.Config.PgPort, configurations.Config.PgUser, configurations.Config.PgPassword, configurations.Config.PgName)
+	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable", configurations.Config.PgHost, configurations.Config.PgPort, configurations.Config.PgUser, configurations.Config.PgPassword, configurations.Config.PgName)
 	//connect to database
-	db, err := sql.Open("postgres", psqlInfo)
+	//db, err := sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL")) // heroku requires to get connection from env variable
 	if err != nil {
 		return err
 	}
