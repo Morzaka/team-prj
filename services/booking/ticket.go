872f6c7ea1ea195c4bf98aa8d2b82f353566317f
Morzaka <vujko007@gmail.com>
2019-04-04 20:16:38 +0300

    service part of CRUD

--- services/booking/ticket.go
+++ services/booking/ticket.go
@@ -2,102 +2,135 @@ package booking
 
 import (
 	"database/sql"
-	"team-project/models/"
+	"encoding/json"
+	"errors"
 	"net/http"
+
+	"team-project/database"
+	"team-project/logger"
+	"team-project/services/data"
+	"team-project/services/model"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	bks, err := models.AllTickets()
+func validateForm(tk data.Ticket) error {
+	if tk.Place == 0 || tk.TicketType == "" || tk.
+		Discount == "" || tk.Price == 0 || tk.TotalPrice == 0 || tk.
+		Name == "" || tk.Surname == "" {
+		return errors.New("all fields must be complete")
+	}
+	return nil
+}
+
+func GetAllTickets(w http.ResponseWriter, r *http.Request) {
+	tkts, err := database.AllTickets()
 	if err != nil {
 		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
 		return
 	}
-
+	w.Header().Set("Content-Type", "application/json")
+	model.JsonEncoding(w, tkts)
 }
 
-func Show(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	bk, err := OneBook(r)
-	switch {
-	case err == sql.ErrNoRows:
-		http.NotFound(w, r)
-		return
-	case err != nil:
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-		return
-	}
-
-	config.TPL.ExecuteTemplate(w, "show.gohtml", bk)
-}
-
-func Create(w http.ResponseWriter, r *http.Request) {
-	config.TPL.ExecuteTemplate(w, "create.gohtml", nil)
-}
-
-func CreateProcess(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	bk, err := PutBook(r)
-	if err != nil {
-		http.Error(w, http.StatusText(406), http.StatusNotAcceptable)
-		return
-	}
-
-	config.TPL.ExecuteTemplate(w, "created.gohtml", bk)
-}
-
-func Update(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	bk, err := OneBook(r)
-	switch {
-	case err == sql.ErrNoRows:
-		http.NotFound(w, r)
-		return
-	case err != nil:
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-		return
-	}
-
-	config.TPL.ExecuteTemplate(w, "update.gohtml", bk)
-}
-
-func UpdateProcess(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	bk, err := UpdateBook(r)
-	if err != nil {
-		http.Error(w, http.StatusText(406), http.StatusBadRequest)
-		return
-	}
-
-	config.TPL.ExecuteTemplate(w, "updated.gohtml", bk)
-}
-
-func DeleteProcess(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	err := DeleteBook(r)
+func GetOneTicket(w http.ResponseWriter, r *http.Request) {
+	id, err := model.GetId(r)
 	if err != nil {
 		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+	}
+
+	tk, err := database.OneTicket(id)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	model.JsonEncoding(w, tk)
+}
+
+func CreateTicket(w http.ResponseWriter, r *http.Request) {
+	// get values from client (json format)
+	tk := data.Ticket{}
+	tk.Id = model.GenerateID()
+	err := json.NewDecoder(r.Body).Decode(&tk)
+	if err != nil {
+		logger.Logger.Errorf("Error, %s", err)
+	}
+
+	// validate form values
+	err = validateForm(tk)
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		w.Write([]byte(string(err.Error())))
 		return
 	}
 
-	http.Redirect(w, r, "/books", http.StatusSeeOther)
+	//validate real number values
+	if tk.Price != float32(tk.Price) || tk.TotalPrice != float32(tk.TotalPrice) {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		_, err = w.Write([]byte(string("Not Acceptable. Price must be a number.")))
+	}
+
+	//insert data to DB 'ticket' table
+	err = database.PutTicket(tk)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		logger.Logger.Errorf("Error, %s", err)
+		return
+	}
+}
+
+func UpdateTicket(w http.ResponseWriter, r *http.Request) {
+	// get values from client (json format)
+	id, err := model.GetId(r)
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+	}
+	tk := data.Ticket{}
+	err = json.NewDecoder(r.Body).Decode(&tk)
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+	}
+
+	tk.Id = id
+
+	// validate form values
+	err = validateForm(tk)
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		w.Write([]byte(string(err.Error())))
+		return
+	}
+
+	//validate real number values
+	if tk.Price != float32(tk.Price) || tk.TotalPrice != float32(tk.TotalPrice) {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		_, err = w.Write([]byte(string("Not Acceptable. Price must be a number.")))
+	}
+
+	err = database.UpdTicket(tk)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		logger.Logger.Errorf("Error, %s", err)
+		return
+	}
+}
+
+func DeleteTicket(w http.ResponseWriter, r *http.Request) {
+	id, err := model.GetId(r)
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+	}
+	err = database.DelTicket(id)
+	if err != nil {
+		logger.Logger.Errorf("Error, %s", err)
+	}
 }
