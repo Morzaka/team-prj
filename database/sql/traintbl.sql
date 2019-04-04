2e94db7cd5548b5939dedf7f33c31c1f5adcab02
supermaaan99 <candyluckone@yahoo.com.ua>
2019-04-04 16:54:56 +0300

    Modified database/sql traintbl.sql

--- database/sql/traintbl.sql
+++ database/sql/traintbl.sql
@@ -1,6 +1,11 @@
 CREATE TABLE public.trains
 (
-    id serial,
-    route bigint,
+    id serial NOT NULL,
+    departure_city text NOT NULL,
+    arrival_city text NOT NULL,
+    departure_time time without time zone,
+    departure_date date,
+    arrival_time time without time zone,
+    arrival_date date,
     PRIMARY KEY (id)
 )
