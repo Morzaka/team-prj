1fb1398c9f7603c3e81b346aceb23500967eaaa2
Bohdan Sydor <bohdansydor2015@gmail.com>
2019-04-04 16:52:42 +0300

    upd plane crud

--- services/plane/plane.go
+++ services/plane/plane.go
@@ -3,7 +3,9 @@ package plane
 import (
 	"database/sql"
 	"fmt"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
+	"team-project/services/model"
 )
 
 const (
@@ -11,10 +13,9 @@ const (
 )
 
 type Plane struct {
-	id    int
-	route string
-	seats int
-	price int
+	id             uuid.UUID
+	departure_city string
+	arrival_city   string
 }
 
 // GetAll is a function for getting all row and column from table
@@ -22,20 +23,22 @@ func GetAll() {
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer db.Close()
 
 	rows, err := db.Query("select * from Planes")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer rows.Close()
 	planes := []Plane{}
 
 	for rows.Next() {
 		p := Plane{}
-		err := rows.Scan(&p.id, &p.route, &p.seats, &p.price)
+		err := rows.Scan(&p.id, &p.departure_city, &p.arrival_city)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -43,41 +46,45 @@ func GetAll() {
 		planes = append(planes, p)
 	}
 	for _, p := range planes {
-		fmt.Println(p.id, p.route, p.seats, p.price)
+		fmt.Println(p.id, p.departure_city, p.arrival_city)
 	}
 }
 
 // GetID is a function for getting row using id
-func GetID(id int) {
+func GetID(id uuid.UUID) {
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer db.Close()
 
 	row := db.QueryRow("select * from Planes where id = $1", id)
 	p := Plane{}
-	err = row.Scan(&p.id, &p.route, &p.seats, &p.price)
+	err = row.Scan(&p.id, &p.departure_city, &p.arrival_city)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
-	fmt.Println(p.id, p.route, p.seats, p.price)
+	fmt.Println(p.id, p.departure_city, p.arrival_city)
 
 }
 
 //Add is a function for adding new row to table
-func Add(route string, seats int, price float64) {
+func Add(departure_city string, arrival_city string) {
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer db.Close()
 
-	result, err := db.Exec("insert into planes (route, seats,price) values ($1, $2, $3)", route, seats, price)
+	result, err := db.Exec("insert into planes (id,departure_city,arrival_city) values ($1, $2, $3)", model.GenerateID(), departure_city, arrival_city)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 
 	fmt.Println(result.RowsAffected())
@@ -85,34 +92,38 @@ func Add(route string, seats int, price float64) {
 }
 
 // Update is a function for updating number of seats in train using id
-func Update(seats int, id int) {
+func Update(arrival_city string, id uuid.UUID) {
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer db.Close()
 
-	result, err := db.Exec("update Planes set seats = $1 where id = $2", seats, id)
+	result, err := db.Exec("update Planes set arrival_city = $1 where id = $2", arrival_city, id)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	fmt.Println(result.RowsAffected())
 
 }
 
 // Delete is a function for deleting row using id
-func Delete(id int) {
+func Delete(id uuid.UUID) {
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer db.Close()
 
 	result, err := db.Exec("delete from Planes where id = $1", id)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	fmt.Println(result.RowsAffected())
 }
