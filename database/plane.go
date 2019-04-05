c1b59baff7a458d6203b9a166473af3f055a4efc
Bohdan Sydor <bohdansydor2015@gmail.com>
2019-04-05 12:57:14 +0300

    move plane.go to database directory

--- database/plane.go
+++ database/plane.go
@@ -1,10 +1,12 @@
-package sql
+package database
 
 import (
 	"github.com/google/uuid"
 	"team-project/database"
+
 	"team-project/services/data"
 )
+
 var (
 	insertPlane = `INSERT INTO public.plane (id,departureCity,arrivalCity)
 	VALUES ($1, $2, $3) returning id`
@@ -15,16 +17,17 @@ var (
 
 func AddPlane(plane data.Plane) (data.Plane, error) {
 	//insert values to the database
-	_, err := database.Db.Exec(insertPlane, plane.)
+	_, err := Db.Exec(insertPlane, plane.)
 	if err != nil {
 		return data.Plane{}, err
 	}
 	return plane, nil
 }
+
 // DeletePlane is a function for deleting row using id
 func DeletePlane(id uuid.UUID) error {
 
-	_ , err := database.Db.Exec(deletePlane, id)
+	_, err := database.Db.Exec(deletePlane, id)
 	if err != nil {
 		return err
 	}
