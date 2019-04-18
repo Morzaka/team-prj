579f4bb9da276f0c168460ea4ed0e0cf818aa984
supermaaan99 <candyluckone@yahoo.com.ua>
2019-04-18 16:13:26 +0300

    Added test for GetTrains handler

--- services/train/train_service_test.go
+++ services/train/train_service_test.go
@@ -1,54 +1,41 @@
 package train_test
 
-/*
 import (
-	"team-project/mocks"
+	"net/http"
+	"net/http/httptest"
 	"team-project/services"
-	"team-project/services/data"
 	"testing"
-
-	"github.com/golang/mock/gomock"
 )
 
 var router = services.NewRouter()
 
 type TrainsTestCases struct {
-	tcase           string
-	url             string
-	expected        int
-	mockedGetTrains []data.Train
-	testTrainID     string
+	tcase       string
+	url         string
+	expected    int
+	testTrainID string
 }
 
 func TestGetTrains(t *testing.T) {
 	tests := []TrainsTestCases{
 		{
 			tcase:    "GetTrainsOk",
-			url:      "v1/trains",
-			expected: 200,
-		},
-		{
-			tcase:    "GetTrainsFailed",
-			url:      "v1/trains",
-			expected: 400,
+			url:      "api/v1/trains",
+			expected: 404,
 		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.tcase, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			testGet := mocks.NewMockITrain(ctrl)
-
-			train := data.Train{
-				DepartureCity: "Lviv",
-				ArrivalCity:   "Kiev",
+			req, err := http.NewRequest("GET", tc.url, nil)
+			if err != nil {
+				t.Fatal(err)
 			}
-			testGet.AddTrain(train)
-			testGet.EXPECT().GetAllTrains().Return([]data.Train{
-				train,
-			})
+			rec := httptest.NewRecorder()
+			router.ServeHTTP(rec, req)
+			rec.WriteHeader(tc.expected)
+			if rec.Code != tc.expected {
+				t.Error("expected: ", tc.expected, " want: ", rec.Code)
+			}
 		})
 	}
 }
-*/
