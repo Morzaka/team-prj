6b78443036ed9ce459260021eb66215bcb490a4d
dmytro <dovgal.dmytr@gmail.com>
2019-04-18 15:47:58 +0300

    db init to local

--- database/database.go
+++ database/database.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"net/url"
 	"os"
+	"team-project/configurations"
 
 	"github.com/go-redis/redis"
 	//pq lib for using postgres
@@ -19,7 +21,12 @@ var (
 
 //PostgresInit connects to postgres database
 func PostgresInit() error {
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL")) // heroku requires to get connection from env variable
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
+		configurations.Config.PgHost, configurations.Config.PgPort, configurations.Config.PgUser, configurations.Config.PgPassword, configurations.Config.PgName)
+	//connect to database
+	db, err := sql.Open("postgres", psqlInfo)
+
 	if err != nil {
 		return err
 	}
